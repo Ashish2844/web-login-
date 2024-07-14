e4cfc25515e6573e2ff47ec1e2f1091f979667e4
Ashish2844 <ashishd595@gmail.com>
2024-07-14 12:36:11 +0530

    Update manageusers.php

--- project8/manageusers.php
+++ project8/manageusers.php
@@ -9,17 +9,6 @@
 </head>
 <body>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-
 
 <div class="header">
 <?php
@@ -132,5 +121,3 @@ msg111=msg11.innerHTML;
 </body>
 </html>
 
-</body>
-</html>
